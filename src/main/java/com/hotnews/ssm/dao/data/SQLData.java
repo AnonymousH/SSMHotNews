4b44d0046279ab651723e0500f607e25c9463d7c
huangjin <2462045166@qq.com>
2017-09-30 16:23:50 +0800

    创建news表和dislike_reason 的关系表并插入数据

--- src/main/java/com/hotnews/ssm/dao/data/SQLData.java
+++ src/main/java/com/hotnews/ssm/dao/data/SQLData.java
@@ -1,6 +1,8 @@
 package com.hotnews.ssm.dao.data;
 
 
+import org.apache.ibatis.annotations.Param;
+
 /**
  * Created by AnonymousH on 2017/9/23
  *
@@ -8,14 +10,32 @@ package com.hotnews.ssm.dao.data;
  */
 public interface SQLData {
 
-
+    /**
+     *  查询类别并插入到新表中
+     * @param position
+     * @return
+     */
     String queryCategory(int position);
 
     void insertCategory(String uid, String category);
 
-
+    /**
+     *  查询不喜欢原因并插入到新表中
+     * @param position
+     * @return
+     */
     String queryDisLike(int position);
 
     void insertDisLikeReason(String uid, String category);
+
+
+    /**
+     *  创建news表和dislike 表的关系表
+     * @param newsID
+     * @param dislikeID
+     */
+    void insertDisLikeRelation(@Param("newsID") long newsID ,@Param("dislikeID") long dislikeID);
+
+    long selectDislikeID(String dislike);
 
 }
--- src/test/java/com/hotnews/ssm/dao/data/SQLDataTest.java
+++ src/test/java/com/hotnews/ssm/dao/data/SQLDataTest.java
@@ -3,6 +3,8 @@ package com.hotnews.ssm.dao.data;
 import com.hotnews.ssm.dao.BaseTest;
 import com.hotnews.ssm.dao.NewsDao;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.*;
@@ -15,10 +17,20 @@ import java.util.*;
  */
 public class SQLDataTest extends BaseTest {
 
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
 
     @Autowired
     SQLData sqlData;
 
+
+    @Test
+    public void test() throws Exception {
+        for (int i = 1; i < 864; i++) {
+            String category =  sqlData.queryCategory(i);
+            logger.info("category------------  "+category);
+        }
+    }
     @Test
     public void insertCategory() throws Exception {
         Set<String> strings = new HashSet<String>();
@@ -74,6 +86,45 @@ public class SQLDataTest extends BaseTest {
             String uid = UUID.randomUUID().toString();
             sqlData.insertDisLikeReason(uid, (String) it.next());
         }
+    }
+
+    @Test
+    public void selectDislikeID() throws Exception {
+        long dislikeID = sqlData.selectDislikeID("山东鲁能");
+        logger.info("dislikeID------------  "+dislikeID);
+    }
+
+    @Test
+    public void insertDisLikeRelation() throws Exception {
+//        Set<String> strings = new HashSet<String>();
+
+        for (int i = 1 ; i < 864; i++) {
+            String category = sqlData.queryDisLike(i);
+
+            category = category.replaceAll("\\[","");
+            category = category.replaceAll("]","");
+
+            if (category != null) {
+
+                List<String> list = Arrays.asList(category.split(","));
+                if (list != null && !list.isEmpty()) {
+                    for (int j = 0; j < list.size(); j++) {
+//                        strings.add(list.get(j).trim());
+                        long dislikeID = sqlData.selectDislikeID(list.get(j).trim());
+                        sqlData.insertDisLikeRelation(i,dislikeID);
+
+                    }
+                }
+
+            }
+
+        }
+
+//        Iterator it = strings.iterator();
+//        while (it.hasNext()) {
+//            String uid = UUID.randomUUID().toString();
+//            sqlData.insertDisLikeReason(uid, (String) it.next());
+//        }
     }
 
 }
